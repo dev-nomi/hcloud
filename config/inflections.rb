702c07b8a66b33f5570811ed7893e3460fea339d
dev-nomi <noman.khalid.freelance@gmail.com>
2022-10-18 22:27:16 +0500

    Remove zeitwerk

--- config/inflections.rb
+++ config/inflections.rb
@@ -1,28 +1,28 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-HCloud.loader.inflector.inflect(
-  "dns_pointer" => "DNSPointer",
-  "floating_ip" => "FloatingIP",
-  "floating_ip_price" => "FloatingIPPrice",
-  "floating_ip_prices" => "FloatingIPPrices",
-  "floating_ips" => "FloatingIPs",
-  "hcloud" => "HCloud",
-  "health_check_http" => "HealthCheckHTTP",
-  "http" => "HTTP",
-  "ip_not_available" => "IPNotAvailable",
-  "ipv4" => "IPv4",
-  "ipv6" => "IPv6",
-  "iso" => "ISO",
-  "iso_type" => "ISOType",
-  "service_http" => "ServiceHTTP",
-  "ssh_key" => "SSHKey",
-  "target_ip" => "TargetIP",
-)
+# HCloud.loader.inflector.inflect(
+#   "dns_pointer" => "DNSPointer",
+#   "floating_ip" => "FloatingIP",
+#   "floating_ip_price" => "FloatingIPPrice",
+#   "floating_ip_prices" => "FloatingIPPrices",
+#   "floating_ips" => "FloatingIPs",
+#   "hcloud" => "HCloud",
+#   "health_check_http" => "HealthCheckHTTP",
+#   "http" => "HTTP",
+#   "ip_not_available" => "IPNotAvailable",
+#   "ipv4" => "IPv4",
+#   "ipv6" => "IPv6",
+#   "iso" => "ISO",
+#   "iso_type" => "ISOType",
+#   "service_http" => "ServiceHTTP",
+#   "ssh_key" => "SSHKey",
+#   "target_ip" => "TargetIP",
+# )
 
-ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.acronym "DNS"
-  inflect.acronym "HTTP"
-  inflect.acronym "IP"
-  inflect.acronym "ISO"
-  inflect.acronym "JSON"
-end
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym "DNS"
+#   inflect.acronym "HTTP"
+#   inflect.acronym "IP"
+#   inflect.acronym "ISO"
+#   inflect.acronym "JSON"
+# end
--- lib/hcloud.rb
+++ lib/hcloud.rb
@@ -2,37 +2,50 @@
 
 require "active_model"
 require "active_support/all"
-require "zeitwerk"
+require 'active_support/dependencies'
+# require "zeitwerk"
 
 require "byebug" if ENV["ENV"] == "development"
 
 module HCloud
   class << self
     # Code loader instance
-    attr_reader :loader
+    # attr_reader :loader
 
     def root
       @root ||= Pathname.new(File.expand_path(File.join("..", ".."), __FILE__))
     end
 
     def setup
-      @loader = Zeitwerk::Loader.for_gem
+      # @loader = Zeitwerk::Loader.for_gem
 
       # Register inflections
-      require root.join("config/inflections.rb")
+      # require root.join("config/inflections.rb")
 
       # Collapse resources
-      loader.collapse(
-        root.join("lib/hcloud/entities"),
-        root.join("lib/hcloud/concerns"),
-        root.join("lib/hcloud/resources"),
-      )
+      # loader.collapse(
+      Dir[root.join('lib/hcloud/entities/*.rb')].each do |f| 
+        require f
+        ActiveSupport::Dependencies.autoload_paths += "#{f}" 
+      end
+      Dir[root.join('lib/hcloud/concerns/*.rb')].each do |f| 
+        require f
+        ActiveSupport::Dependencies.autoload_paths += "#{f}" 
+      end
+      Dir[root.join('lib/hcloud/resources/*.rb')].each do |f| 
+        require f
+        ActiveSupport::Dependencies.autoload_paths += "#{f}" 
+      end
+      Dir[root.join('lib/hcloud/*.rb')].each do |f| 
+        require f
+        ActiveSupport::Dependencies.autoload_paths += "#{f}" 
+      end
 
       # Load types
       require root.join("lib/hcloud/resource_type")
 
-      loader.setup
-      loader.eager_load
+      # loader.setup
+      # loader.eager_load
     end
   end
 end
